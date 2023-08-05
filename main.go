671832fbf762b92a01a71f6619a0d9ae872c9d92
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-08-04 22:17:53 -0300

    key creation

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/kms"
+	"github.com/aws/aws-sdk-go-v2/service/kms/types"
 )
 
 func main() {
@@ -14,5 +16,23 @@ func main() {
 	}
 	client := kms.NewFromConfig(cfg)
 
-	input := &kms.CreateKeyInput{}
+	description := "external-cmk"
+	tagKey := "Name"
+	tagValue := "external-cmk"
+	tags := []types.Tag{{TagKey: &tagKey, TagValue: &tagValue}}
+
+	input := &kms.CreateKeyInput{
+		Description: &description,
+		Tags:        tags,
+	}
+
+	result, err := client.CreateKey(context.TODO(), input)
+
+	if err != nil {
+		fmt.Println("Got error creating key:")
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(*result.KeyMetadata.KeyId)
 }
